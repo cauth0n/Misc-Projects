da9b573d8067329d24aff22eb5313a0b202db2e8
Derek Reimanis <derekreimanis@yahoo.com>
2012-09-01 12:46:55 -0600

    First Push
    Game of Konane

---
+++ Konane/src/Game/MiniMaxWithoutAB.java
@@ -0,0 +1,28 @@
+package Game;
+
+public class MiniMaxWithoutAB extends Player {
+
+	public MiniMaxWithoutAB(String colorValue) {
+		super(colorValue);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getPlayerType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getColorValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setColorValue(String colorValue) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Konane/src/Game/Tile.java
@@ -0,0 +1,20 @@
+package Game;
+
+public class Tile {
+	
+	private String colorValue;
+	
+	public Tile(String colorValue){
+		this.colorValue = colorValue;
+	
+	}
+
+	public String getColorValue() {
+		return colorValue;
+	}
+
+	public void setColorValue(String colorValue) {
+		this.colorValue = colorValue;
+	}
+
+}
